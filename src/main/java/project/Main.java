41d766eaf6af1a85fac1fab20e1362421cf349e4
Weiß Susanne <sw191@hdm-stuttgart.de>
2019-01-08 16:50:27 +0100

    Auswahl 0

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
         String toBuy = "not set";
         String toSell = "not set";
         String top = "Currency to buy: " + toBuy + "\nCurrency to sell: " + toSell + "\n++++++++++++++++++++++++++++";
@@ -28,26 +31,6 @@ public class Main {
 
         ArrayList<Currency> currencylist = new ArrayList<>();
 
-        System.out.println(top+"\n"+auswahlStart+"\n\n\n"+exitStart);
-
-        Scanner eingabe = new Scanner(System.in);
-
-        while (eingabe.hasNext()) {
-
-            if (eingabe.nextInt()==0) {
-
-            } else if (eingabe.nextInt()==1) {
-
-            } else if (eingabe.nextInt()==2) {
-
-            } else if (eingabe.next()=="x") {
-
-            } else {
-                System.out.println("ungültige Eingabe!");
-                eingabe.next();
-            }
-        }
-
         try {
             Scanner s = new Scanner(file);
             while (s.hasNext()) {
@@ -64,12 +47,60 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        System.out.println(top+"\n"+auswahlStart+"\n\n\n"+exitStart);
+
+        Scanner eingabe = new Scanner(System.in);
+
+        while (eingabe.hasNext()&& auswahlGesetzt==false) {
+            String e = eingabe.next();
+
+            if (e.equals("0")) {
+                auswahlGesetzt = true;
+
+                System.out.println(top+"\n"+auswahlStart+"\n\n\n"+exitStart+"\n"+auswahl);
+
+                e = eingabe.next();
+
+                int j = 0;
+                String[] vorschlag = new String[50];
+                for(int i = 0; i<50; i++){
+
+                    String a = currencylist.get(i).getName();
+
+                    if (a.contains(e)) {
+                        vorschlag [j] = a;
+                        System.out.println(j + ": " + vorschlag[j]);
+                        j++;
+
+                    }
+                }
+                e = eingabe.next();
+
+                for(int i = 0; i<vorschlag.length; i++)
+
+                    if(e.equals(""+i)){
+                        System.out.println("Depp");
+                    }
+
+            } else if (e.equals("1")) {
+                auswahlGesetzt = true;
+
+            } else if (e.equals("2")) {
+                auswahlGesetzt = true;
+
+            } else if (e.equals("x")) {
+                auswahlGesetzt = true;
+
+            } else {
+                System.out.println("ungültige Eingabe!");
+                eingabe.next();
+            }
+        }
+
+
     }
 
-    private static String[] Vorschlaege (String s, ArrayList a){
-        String [] b = new String[50];
-        return b;
-    }
 
     private static double Umrechner (String buy, String sell){
         return 1.2;
