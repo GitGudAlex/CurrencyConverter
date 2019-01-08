23c59870a28103591df0068f023f5f43eaa13530
Weiß Susanne <sw191@hdm-stuttgart.de>
2019-01-08 11:35:16 +0100

    If-Abfrage erste Versuche

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -15,13 +15,38 @@ public class Main {
         String auswahl = "Enter a currency´s name or part of it (>>xxx<< to exit):";
         String select = "Select a currency by index:";
         String auswahl2 = "Enter an amount:";
+        String c;
+
+        int b = 0;
+
+        boolean zweiAusgewählt = false;
+        boolean auswahlGesetzt = false;
+
 
         File file = new File("currencies.csv");
 
+
         ArrayList<Currency> currencylist = new ArrayList<>();
 
-        String c;
-        int b = 0;
+        System.out.println(top+"\n"+auswahlStart+"\n\n\n"+exitStart);
+
+        Scanner eingabe = new Scanner(System.in);
+
+        while (eingabe.hasNext()) {
+
+            if (eingabe.nextInt()==0) {
+
+            } else if (eingabe.nextInt()==1) {
+
+            } else if (eingabe.nextInt()==2) {
+
+            } else if (eingabe.next()=="x") {
+
+            } else {
+                System.out.println("ungültige Eingabe!");
+                eingabe.next();
+            }
+        }
 
         try {
             Scanner s = new Scanner(file);
@@ -31,7 +56,7 @@ public class Main {
                 String a[] = c.split(":");
                 Currency neu = new Currency (a[0],a[1]);
                 currencylist.add(b, neu);
-                System.out.println(currencylist.get(b).getRate());
+                //System.out.println(currencylist.get(b).getRate());
                 b++;
 
 
@@ -39,5 +64,14 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String[] Vorschlaege (String s, ArrayList a){
+        String [] b = new String[50];
+        return b;
+    }
+
+    private static double Umrechner (String buy, String sell){
+        return 1.2;
     }
 }
