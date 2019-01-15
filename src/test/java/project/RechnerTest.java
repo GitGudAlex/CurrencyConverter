08699841948ed94d86f34d3be3e93cec4806d4f9
Weiß Susanne <sw191@hdm-stuttgart.de>
2019-01-15 22:05:39 +0100

    Unit Tests der Methode Umrechner ergänzt, Unit Tests für die Methode PunktKomma erstellt

--- src/test/java/project/RechnerTest.java
+++ src/test/java/project/RechnerTest.java
@@ -11,9 +11,19 @@ public class RechnerTest {
     public void umrechner() {
         String EuroMexicanPeso [] = {"26.381300", "1.273760"};
         String EuroUSDollar [] = {"1.349170", "1.273760"};
+        String BahrainDinarKazakhstaniTenge [] = {"426.687000", "0.507287"};
+        String KazakhstaniTengeBahrainDinar [] = {"0.507287", "426.687000"};
+
         Assert.assertEquals(72.49, Rechner.Umrechner(EuroMexicanPeso, 3.5), 1e-15);
+        Assert.assertEquals(2.07113584977E9, Rechner.Umrechner(EuroMexicanPeso, 1.0E8), 1e-15);
         Assert.assertEquals(5.3, Rechner.Umrechner(EuroUSDollar, 5.0), 1e-15);
         Assert.assertEquals(0.0, Rechner.Umrechner(EuroUSDollar, 0.0), 1e-15);
+        Assert.assertEquals(105920.27, Rechner.Umrechner(EuroUSDollar, 100000.0), 1e-15);
+        Assert.assertEquals(0.0, Rechner.Umrechner(KazakhstaniTengeBahrainDinar, 1.0), 1e-15);
+        Assert.assertEquals(118.89, Rechner.Umrechner(KazakhstaniTengeBahrainDinar, 100000.0), 1e-15);
+        Assert.assertEquals(8.411155815E7, Rechner.Umrechner(BahrainDinarKazakhstaniTenge, 100000.0), 1e-15);
+        Assert.assertEquals(-8411.16, Rechner.Umrechner(BahrainDinarKazakhstaniTenge, -10.0), 1e-15);
+
     }
 
     @Test
@@ -23,9 +33,14 @@ public class RechnerTest {
 
     @Test
     public void blockZwei() {
+        //Assert.assertEquals(5.3 , Rechner.BlockZwei("U.S. dollar", "Euro", 5.0), 1e-15);
     }
 
     @Test
     public void punktKomma() {
+        Assert.assertEquals(3.4, Rechner.PunktKomma("3.4"), 1e-15);
+        Assert.assertEquals(3.4, Rechner.PunktKomma("3,4"), 1e-15);
+        Assert.assertEquals(5463.9921, Rechner.PunktKomma("5463.9921"), 1e-15);
+        Assert.assertEquals(5463.9921, Rechner.PunktKomma("5463,9921"), 1e-15);
     }
 }
