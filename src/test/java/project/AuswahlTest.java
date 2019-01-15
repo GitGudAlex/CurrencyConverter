fd605be320c2eb7a3daa5056e95b9d203adad5ab
Weiß Susanne <sw191@hdm-stuttgart.de>
2019-01-15 21:13:50 +0100

    UnitTest für Methode ausgewählt und Methode Umrechner

--- src/test/java/project/AuswahlTest.java
+++ src/test/java/project/AuswahlTest.java
@@ -15,13 +15,20 @@ public class AuswahlTest {
 
     @Test
     public void möglichkeitenAuswahl() {
-        String expected [] = {"U.S. dollar", "Australian dollar", "Brunei dollar", "Canadian dollar", "New Zealand dollar", "Singapore dollar", "Trinidadian dollar" };
-
-        Assert.assertArrayEquals(expected, Auswahl.MöglichkeitenAuswahl("dol"));
     }
 
     @Test
     public void ausgewaehlt () {
+        String [] vorschlagEuro = {"Euro"};
+        String [] vorschlagDol = {"U.S. dollar", "Australian dollar", "Brunei dollar", "Canadian dollar", "New Zealand dollar", "Singapore dollar", "Trinidadian dollar"};
+        String [] vorschlagPeso = {"Chilean peso", "Mexican peso", "Philippine peso", "Uruguayan peso"};
+        Assert.assertEquals("not set", Auswahl.ausgewaehlt("xxx", vorschlagEuro, "0", "not set", "not set"));
+        Assert.assertEquals("not set", Auswahl.ausgewaehlt("xxx", vorschlagDol, "0", "not set", "not set"));
+        Assert.assertEquals("not set", Auswahl.ausgewaehlt("xxx", vorschlagDol, "1", "not set", "not set"));
+        Assert.assertEquals("Australian dollar", Auswahl.ausgewaehlt("1", vorschlagDol, "0", "not set", "not set"));
+        Assert.assertEquals("Singapore dollar", Auswahl.ausgewaehlt("5", vorschlagDol, "0", "not set", "not set"));
+        Assert.assertEquals("Chilean peso", Auswahl.ausgewaehlt("0", vorschlagPeso, "0", "Euro", "U.S. dollar"));
+        Assert.assertEquals("Chilean peso", Auswahl.ausgewaehlt("0", vorschlagPeso, "1", "New Zealand dollar", "Trinidadian dollar"));
 
     }
 }
--- src/test/java/project/RechnerTest.java
+++ src/test/java/project/RechnerTest.java
@@ -9,11 +9,16 @@ public class RechnerTest {
 
     @Test
     public void umrechner() {
+        String EuroMexicanPeso [] = {"26.381300", "1.273760"};
+        String EuroUSDollar [] = {"1.349170", "1.273760"};
+        Assert.assertEquals(72.49, Rechner.Umrechner(EuroMexicanPeso, 3.5), 1e-15);
+        Assert.assertEquals(5.3, Rechner.Umrechner(EuroUSDollar, 5.0), 1e-15);
+        Assert.assertEquals(0.0, Rechner.Umrechner(EuroUSDollar, 0.0), 1e-15);
     }
 
     @Test
     public void SDRWert() {
-        Assert.assertArrayEquals(new String[]{"1.273760", "1.349170"}, Rechner.SDRWert("U.S. dollar", "Euro"));
+        //Assert.assertArrayEquals(new String[]{"1.273760", "1.349170"}, Rechner.SDRWert("U.S. dollar", "Euro"));
     }
 
     @Test
