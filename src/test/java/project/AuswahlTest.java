610c79fee6a69f0e9ffb8fc5195f33bf3d507058
Weiß Susanne <sw191@hdm-stuttgart.de>
2019-01-16 11:15:17 +0100

    UnitTest Klasse Auswahl, Methode MöglichkeitenAuswahl

--- src/test/java/project/AuswahlTest.java
+++ src/test/java/project/AuswahlTest.java
@@ -15,6 +15,36 @@ public class AuswahlTest {
 
     @Test
     public void möglichkeitenAuswahl() {
+        Main.currencyListeFüllen();
+        String [] vorschlagDol = new String [Main.currencylist.size()];
+        String [] vorschlagEuro = new String [Main.currencylist.size()];
+        String [] vorschlagAu = new String [Main.currencylist.size()];
+        String [] vorschlagNew = new String [Main.currencylist.size()];
+
+        vorschlagDol [0] = "U.S. dollar";
+        vorschlagDol [1] = "Australian dollar";
+        vorschlagDol [2] = "Brunei dollar";
+        vorschlagDol [3] = "Canadian dollar";
+        vorschlagDol [4] = "New Zealand dollar";
+        vorschlagDol [5] = "Singapore dollar";
+        vorschlagDol [6] = "Trinidadian dollar";
+
+        vorschlagEuro [0] = "Euro";
+
+        vorschlagAu [0] = "Australian dollar";
+        vorschlagAu [1] = "Mauritian rupee";
+        vorschlagAu [2] = "Saudi Arabian riyal";
+
+        vorschlagNew [0] = "Israeli New Shekel";
+        vorschlagNew [1] = "New Zealand dollar";
+
+        Assert.assertArrayEquals(vorschlagDol, Auswahl.MöglichkeitenAuswahl("dol"));
+        Assert.assertArrayEquals(vorschlagEuro, Auswahl.MöglichkeitenAuswahl("Eu"));
+        Assert.assertArrayEquals(vorschlagEuro, Auswahl.MöglichkeitenAuswahl("eur"));
+        Assert.assertArrayEquals(vorschlagEuro, Auswahl.MöglichkeitenAuswahl("Euro"));
+        Assert.assertArrayEquals(vorschlagAu, Auswahl.MöglichkeitenAuswahl("au"));
+        Assert.assertArrayEquals(vorschlagAu, Auswahl.MöglichkeitenAuswahl("Au"));
+        Assert.assertArrayEquals(vorschlagNew, Auswahl.MöglichkeitenAuswahl("new"));
     }
 
     @Test
